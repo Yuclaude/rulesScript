edaff053c13afb565a385f8eec23a215dc0d1370
Yuclaude <162446397+Yuclaude@users.noreply.github.com>
2024-09-07 18:55:12 +0800

    更新 AppRaven.js

--- QX/js/AppRaven.js
+++ QX/js/AppRaven.js
@@ -6,7 +6,7 @@ AppRaven
 [rewrite_local]
 
 
-https://appraven.net/appraven/graphql url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/AppRaven.js
+https://appraven.net/appraven/graphql url script-response-body https://raw.githubusercontent.com/Yuclaude/rewrite/main/QX/js/AppRaven.js
 [mitm] 
 
 hostname = appraven.net
