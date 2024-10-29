a06c710d2e72e6f9f0410d5efc2c684a2b6776e3
Yuclaude <162446397+Yuclaude@users.noreply.github.com>
2024-10-29 19:02:07 +0800

    更新 shadowshareGet.js

--- QX/node/shadowshareGet.js
+++ QX/node/shadowshareGet.js
@@ -1,11 +1,13 @@
 /*
 https://raw.githubusercontent.com/Yuclaude/rewrite/main/QX/node/shadowshareGet.js
 
-*/
 
+[rewrite_local]
+https://gitee.com/api/v5/repos/c0nfigshare/share/contents url script-response-body https://raw.githubusercontent.com/Yuclaude/rewrite/main/QX/node/shadowshareGet.js
 
 [MITM]
 hostname = gitee.com
+*/
 
 
 let obj = JSON.parse($response.body);
