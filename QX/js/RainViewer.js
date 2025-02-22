de570397d0864e3c501f6db42b746d0525a1d927
Yuclaude <162446397+Yuclaude@users.noreply.github.com>
2025-02-22 23:41:41 +0800

    更新 RainViewer.js

--- QX/js/RainViewer.js
+++ QX/js/RainViewer.js
@@ -2,7 +2,7 @@
 
 项目名称：RainViewer天气预报
 下载地址：https://t.cn/A6WqTbgz
-脚本作者：chxm1023
+脚本作者：chxm1023 https://raw.githubusercontent.com/chxm1023/Rewrite/main/RainViewer.js
 电报频道：https://t.me/chxm1023
 使用声明：⚠️仅供参考，🈲转载与售卖！
 
