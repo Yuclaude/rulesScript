18cd00ca56244e65637c39e3cd793cbdeb0e4778
Yuclaude <162446397+Yuclaude@users.noreply.github.com>
2025-07-15 19:10:20 +0800

    更新 danmuConfig.js

--- uzVideo/danmuConfig.js
+++ uzVideo/danmuConfig.js
@@ -1,6 +1,8 @@
 {
     "name": "弹幕扩展docker",
     "version": 3,
+    "env": "弹幕线路##格式 线路名称1@地址1;线路名称2@地址2",
+    "order": "A01",
     "api": "https://raw.githubusercontent.com/Yuclaude/rulesScript/refs/heads/master/uzVideo/danmu.js",
     "type": 400
   }
